023659e618c23fe5fae78853d7c22d2c3257002b
brianticona <bri.ticonam@gmail.com>
2026-04-19 14:43:09 -0300

    ej 1

---
+++ TP4 ej. 1/src/tp4/ej/pkg1/TP4Ej1.java
@@ -0,0 +1,18 @@
+package tp4.ej.pkg1;
+import java.util.Scanner;
+public class TP4Ej1 {
+    public static void main(String[] args) {
+        Scanner n = new Scanner(System.in);
+        int [] num = new int [7];
+        int i = 0;
+        System.out.println("Ingrese 7 numeros enteros:");
+        while (i < 7){
+            num[i] = n.nextInt();
+            i++;
+        }
+        System.out.println("Los 7 numeros enteros ingresados fueron:");
+        for (int c = 0; c < 7; c++){
+            System.out.println(num[c]);
+        }
+    }
+}
