c90d90c3dbbd4705bdf8041ac05c20bb1d37558f
brianticona <bri.ticonam@gmail.com>
2026-05-14 18:15:07 -0300

    uso un array de booleans en el punto 9

--- TP4 ej. 9/src/tp4/ej/pkg9/TP4Ej9.java
+++ TP4 ej. 9/src/tp4/ej/pkg9/TP4Ej9.java
@@ -3,19 +3,23 @@ import java.util.Scanner;
 public class TP4Ej9 {
     public static void main(String[] args) {
         Scanner n = new Scanner(System.in);
-        int[] p = new int[10];
+        boolean[] p = new boolean[10];
         int cp = 0;
+        int r = 0;
         for (int i = 0; i < 10; i++) {
-            System.out.println("El alumno " + (i + 1) + " pago?:");
-            p[i] = n.nextInt();
-            if (p[i] == 1) {
+            System.out.println("El alumno " + (i + 1) + " pago la cuota?:");
+            r = n.nextInt();
+            if (r == 1) {
+                p[i] = true;
                 cp++;
+            } else {
+                p[i] = false;
             }
         }
         int cd = 10 - cp;
         double pr = (cp * 100.0) / 10;
-        System.out.println("Cantidad de alumnos que pagaron: " + cp);
-        System.out.println("Cantidad de alumnos que adeudan: " + cd);
-        System.out.println("Porcentaje de alumnos que pagaron: " + pr + "%");
+        System.out.println("Cantidad de pagos: " + cp);
+        System.out.println("Cantidad de deudas: " + cd);
+        System.out.println("Porcentaje de cobranza: " + pr + "%");
     }
 }
